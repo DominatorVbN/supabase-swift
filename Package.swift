83eab57da97f5e8664dedd5e2614a5dc3829e562
Amit Samant <amit.samant@gojek.com>
2023-10-08 21:51:11 +0530

    Used DominatorVbN version of postgrest-swift

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ if ProcessInfo.processInfo.environment["USE_LOCAL_PACKAGES"] != nil {
       .package(url: "https://github.com/supabase-community/gotrue-swift", from: "1.0.0"),
       .package(url: "https://github.com/supabase-community/storage-swift.git", from: "0.1.1"),
       .package(url: "https://github.com/supabase-community/realtime-swift.git", from: "0.0.2"),
-      .package(url: "https://github.com/supabase-community/postgrest-swift", from: "1.0.0"),
+      .package(url: "https://github.com/DominatorVbN/postgrest-swift.git", branch: "master"),
       .package(url: "https://github.com/supabase-community/functions-swift", from: "1.0.0"),
     ]
   )
